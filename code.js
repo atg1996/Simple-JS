831800299b88d91ce9d7c968e006332345e44e82
atg1996 <t.asatryan96@gmail.com>
2017-05-19 01:21:14 +0400

    Simple JS

---
+++ code.js
@@ -0,0 +1,46 @@
+/*if (true) {
+  var name = '123';
+}
+console.log(name);
+*/
+
+/*
+if (true) {
+  let name = '123';
+}
+console.log(name);
+*/
+
+/*
+for (var i=0; i<3; i++) {
+  console.log(i);
+}
+console.log(i);
+*/
+/*
+if (5<3) {
+  console.log('this is impossible');
+} else{
+  console.log('really 5 is not less than 3' );
+}
+*/
+
+/*endless cycle
+let i=0;
+while (i < 4) {
+  console.log(2);
+}
+*/
+/*array basic example
+let students = ['student1','student2','student3']
+console.log(students[1]);
+*/
+/* not an error, but undefined
+let students = ['student1','student2','student3']
+console.log(students[40]);
+*/
+/* container
+const friends = {friend1: '123456' , friend2: '789456'};
+console.log(friends.friend1);
+console.log(friends['friend2']);
+*/
